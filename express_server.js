7f6185e6c0999403118ddf1ecc6104a28c9b17b4
peterhangg <peterhanggg@gmail.com>
2019-09-30 19:29:25 +0000

    instslled body-parser

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
+//// reqired npm library ////
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -34,6 +37,7 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  // console.log("this is the shortURL req.params:", req.params.shortURL);
   res.render("urls_show", templateVars);
 });
 

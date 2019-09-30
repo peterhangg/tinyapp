aabf2416cf9b28230f85f6b030a4611e89ff4a9c
peterhangg <peterhanggg@gmail.com>
2019-09-30 18:32:20 +0000

    created views folder and urls_index.ejs template

--- express_server.js
+++ express_server.js
@@ -19,8 +19,14 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase };
+  res.render("urls_index", templateVars); 
+})
 
 //// listen ////
 app.listen(PORT, () => {

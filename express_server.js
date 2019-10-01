2db903513147ebe1efb148f4fc6f737c7c7bf50c
peterhangg <peterhanggg@gmail.com>
2019-10-01 03:05:04 +0000

    added delete button, fix header and added comments

--- express_server.js
+++ express_server.js
@@ -23,11 +23,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-//// get request ////
+/////////// GET REQUEST ////////////
 app.get("/", (req, res) => {  // "/" is our root directory
   res.send("Hello!");
 });
 
+// get request to urlDatabase
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -37,27 +38,31 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
+// get request to urls
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase };
   res.render("urls_index", templateVars); 
 })
 
+// get request to creating new urls
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// get request to shortURLs
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   // console.log("this is the shortURL req.params:", req.params.shortURL);
   res.render("urls_show", templateVars);
 });
 
+// get request longURL redirect 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-//// post request ////
+///////// POST REQUEST /////////
 app.post("/urls", (req, res) => {
   console.log("This is the longURL link", req.body.longURL);
   if(!req.body.longURL.includes("http://")) { // append http to longURL

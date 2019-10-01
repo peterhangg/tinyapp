fae8420b6f8f7fdb9a1880b0ebb99555883c9f5b
peterhangg <peterhanggg@gmail.com>
2019-10-01 21:20:33 +0000

    fixed accessing other links on the page when logged in as user

--- express_server.js
+++ express_server.js
@@ -42,18 +42,19 @@ app.get("/hello", (req, res) => {
 
 // get request to urls
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase,  username: req.cookies["username"] };
+  let templateVars = { urls: urlDatabase,  username: req.cookies["username"] };
   res.render("urls_index", templateVars); 
 })
 
 // get request to creating new urls
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+ let templateVars = { urls: urlDatabase,  username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 // get request to shortURLs
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   // console.log("this is the shortURL req.params:", req.params.shortURL);
   res.render("urls_show", templateVars);
 });

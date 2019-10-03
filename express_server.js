3b7bb805cb68429f12616dc5135cebb8f5ede544
peterhangg <peterhanggg@gmail.com>
2019-10-03 04:57:03 +0000

    fix non logged-in users editting URLs, fix comments and linted

--- express_server.js
+++ express_server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
-  keys: ["545433"],
+  keys: ["key"],
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
@@ -55,7 +55,7 @@ const idLookup = (email) => {
 
 const urlsForUser = (id) => {
   let filteredURL = {};
-  for (urls in urlDatabase) {
+  for (let urls in urlDatabase) {
     if (urlDatabase[urls]["userID"] === id) {
       filteredURL[urls] = urlDatabase[urls];
     }
@@ -68,21 +68,21 @@ const urlDatabase = {
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: bcrypt.hashSync("purple-monkey-dinosaur",10)
   },
- "aJ48lW": {
-    id: "aJ48lW", 
-    email: "a@a.com", 
+  "aJ48lW": {
+    id: "aJ48lW",
+    email: "a@a.com",
     password: bcrypt.hashSync("123", 10)
   }
-}
+};
 
 /////////// GET REQUEST ////////////
-app.get("/", (req, res) => {  // "/" is our root directory
+app.get("/", (req, res) => {  // 
   res.send("Hello!");
 });
 
@@ -96,60 +96,59 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
-// get request to urls
+// access "My URLs" page
 app.get("/urls", (req, res) => {
-  let templateVars = { 
-    urls: urlsForUser(req.session.user_id),  
+  let templateVars = {
+    urls: urlsForUser(req.session.user_id),
     user: users[req.session.user_id],
   };
-
   // console.log(templateVars);
   res.render("urls_index", templateVars);
 });
 
-// get request to creating new urls
+// access page to create "new URLs"
 app.get("/urls/new", (req, res) => {
   let templateVars = { urls: urlDatabase,  user: users[req.session.user_id] };
-  if(!req.session.user_id) {
+  if (!req.session.user_id) {
     res.redirect("/login");
     return;
   }
   res.render("urls_new", templateVars);
 });
 
-// get request to shortURLs
+// access page that displays shortURLs with their corresponding longURLs
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.session.user_id] };
   // console.log("this is the shortURL req.params:", req.params.shortURL);
-  console.log(templateVars)
+  console.log(templateVars);
   res.render("urls_show", templateVars);
 });
 
-// get request longURL redirect
+//load the webpage that is linked to it's shortURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
-// registration page 
+// access registration page
 app.get("/register", (req, res) => {
   let templateVars = { urls: urlDatabase,  user: users[req.session.user_id] };
   res.render("register", templateVars);
 });
 
-// login page
+// access login page
 app.get("/login", (req, res) => {
   let templateVars = { urls: urlDatabase,  user: users[req.session.user_id] };
   res.render("login", templateVars);
 });
 
 ///////// POST REQUEST /////////
+// Add new URLs to the database
 app.post("/urls", (req, res) => {
   const newShortURL = generateRandomString();
   if (!req.body.longURL.includes("http://")) { // append http to longURL
     req.body.longURL = `http://${req.body.longURL}`;
   }
-  console.log(req.body.longURL);
 
   urlDatabase[newShortURL] = { longURL: req.body.longURL, userID: req.session.user_id }; // adds a new shortURL to the urlDatabase when submitted in our form
   console.log(urlDatabase);
@@ -157,36 +156,38 @@ app.post("/urls", (req, res) => {
   // console.log(urlDatabase);
 });
 
-// delete urls
+// delete URLs from the "My URLs" page
 app.post("/urls/:shortURL/delete", (req, res) => {
   if (!req.session.user_id) {
-    res.send("Must login before deleting URL.")
+    res.send("Must login before deleting URL.");
     return;
-  };
+  }
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
-// edit urls
+// edit longURLs that correspond with it's shortURL
 app.post("/urls/:id", (req, res) => {
   let editURL = req.body.editURL;
-  if (!editURL.includes("http://")) { // append http to longURL
-  editURL = `http://${editURL}`;
-}
-  if (req.session.user_id) {
+  if (!editURL.includes("http://")) { // append http:// to longURL
+    editURL = `http://${editURL}`;
+  }
+  if (!req.session.user_id) {
+    res.send("Only logged-in users can edit URLs!");
+  } else {
     urlDatabase[req.params.id].longURL = editURL;
-  };
+  }
   res.redirect(`/urls`);
 });
 
-// Login route
+// login credentials 
 app.post("/login", (req, res) =>{
   const email = req.body.email;
   const password = req.body.password;
   if (!emailCheck(email)) {
-    res.status(403).send("Incorrect email");
+    res.status(403).send("Email is not valid!");
   } else if (!passwordcheck(password)) {
-    res.status(403).send("Inocrrect password");
+    res.status(403).send("Invalid password");
   } else {
     req.session.user_id = idLookup(email);
     console.log("user logging in ----->", idLookup(email));
@@ -196,7 +197,7 @@ app.post("/login", (req, res) =>{
   res.redirect("/urls");
 });
 
-// Logout route
+// logout user
 app.post("/logout", (req, res) =>{
   req.session = null;
   res.redirect("/urls");
@@ -205,19 +206,19 @@ app.post("/logout", (req, res) =>{
 // handles registation form data
 app.post("/register", (req, res) => {
   const email = req.body.email;
-  const password = bcrypt.hashSync(req.body.password, 10);
+  const password = req.body.password;
   const id =  generateRandomString();
 
-  if(!email || !password) {
+  if (!email || !password) {
     res.status(400).send("Please fill out the registation form as it is required.");
   } else if (emailCheck(email)) {
     res.status(400).send("This email already exist! Try again.");
   } else {
-    users[id] = { id , email, password };
+    users[id] = { id , email, password: bcrypt.hashSync(password, 10) };
     // console.log("User object:",users);
     console.log("user register", id);
     res.cookie("user_id", id);
-    console.log(users)
+    console.log(users);
     res.redirect("/urls");
   }
 });
